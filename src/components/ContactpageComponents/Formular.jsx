3bd54a037665776c0c477eeec052560c307fcd3b
Kanza Bokhari <kanza.bokhari@akqa.com>
2023-05-28 21:41:00 +0200

    Contactpage responsivgt men mangler funktionalitet

--- src/components/ContactpageComponents/Formular.jsx
+++ src/components/ContactpageComponents/Formular.jsx
@@ -32,17 +32,29 @@ function Formular() {
 
   return (
     <div className="">
-      <div className="content text-white sm:pt-24 ">
+      <div className="content text-white sm:pt-40 md:pt-20">
         <div className="top-elements flex flex-col items-center">
-          <video src={CheersAni} muted autoPlay className="sm:w-1/2"></video>
-          <h1 className="font-Ermitial sm:text-6xl">CONTACT US</h1>
+          <video
+            src={CheersAni}
+            muted
+            autoPlay
+            className="sm:w-1/2 lg:w-1/3 xl:w-1/3"
+          ></video>
+          <h1 className="font-Ermitial sm:text-5xl md:text-7xl lg:text-8xl xl:text-8xl">
+            CHEERS WITH US
+          </h1>
+          <h2 className="text-center font-AvenirMedium md:text-2xl lg:text-3xl">
+            Contact us on{" "}
+            <span className="underline">orders@petitverre.dk</span> or fill the
+            form below
+          </h2>
         </div>
 
-        <div className="Form sm:pt-20">
+        <div className="Form sm:pt-20 md:pt-28 lg:pt-32 xl:pt-40 lg:px-10 ">
           <form
             ref={form}
             onSubmit={sendEmail}
-            className=" flex flex-col sm:space-y-5 "
+            className=" flex flex-col sm:space-y-5 md:px-20 lg:px-52 xl:px-96"
           >
             <div className="nameDiv flex flex-col sm:px-10">
               <label className="text-grey font-AvenirMedium sm:text-xl">
@@ -85,13 +97,13 @@ function Formular() {
               />
             </div>
 
-            <div className="sendButton sm:pt-10 flex flex-col items-center">
+            <div className="sendButton sm:pt-10 md:pt-10 lg:pt-10 flex flex-col items-center">
               <CTAbutton buttonText="SEND" type="submit" value="Send" />
             </div>
           </form>
         </div>
 
-        <div className="soMe flex flex-row sm:justify-center items-center sm:pt-24 sm:pr-5 sm:pb-5 sm:space-x-3">
+        <div className="soMe flex flex-row sm:justify-center items-center sm:pt-14 sm:pr-5 sm:pb-10 sm:space-x-3 md:pb-10 md:pt20 lg:justify-end lg:pr-14 xl:pr-10">
           <div className="icon">
             <img src={igIcon} alt="iconInstagram" className="sm:max-w-xs" />
           </div>
@@ -101,7 +113,7 @@ function Formular() {
               target="_blank"
               rel="noreferrer"
             >
-              <p className="sm:text-xl">petitverredk</p>
+              <p className="sm:text-xl underline">@petitverredk</p>
             </a>
           </div>
         </div>
